78ef50d9870034639752a0bda169e6cd1049e4b9
Jeff B <Jeff.BozierJr@gmail.com>
2023-06-27 23:44:12 -0400

    completing the photo slideshow.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,25 @@
 import React from 'react'
 import "./dashboard.css"
+import 'react-slideshow-image/dist/styles.css'
+import { Fade } from 'react-slideshow-image';
+import img1 from '../assets/imgA.jpg'
+import img2 from '../assets/imgB.jpg'
+import img3 from '../assets/imgC.jpg'
+
+const fadeImages = [
+  {
+    img: img1,
+    caption: "love you"
+  },
+  {
+    img: img2,
+    caption: "beautiful lady"
+  },
+  {
+    img: img3,
+    caption: "My Boo"
+  }
+]
 const Dashboard = () => {
   return (
     <>
@@ -12,9 +32,19 @@ const Dashboard = () => {
     <h1 id='dashboardTitle'>6:30pm - 11:30pm</h1>
 
     <div className="dashboardContainer">
+
+      <div id="galleryContainer">
+        {/* import a feature to have a gallery for all of the photos */}
+      </div>
         <div id="dashboardSlideshow">
-            {/* slideshow of pictures cycling through  */}
-            {/* ability to look at the pictures and to download them */}
+          <Fade>
+            {fadeImages.map((fadeImage, index) => (
+              <div key={index}  style={{ border: "2px solid blue"}}>
+                <img style={{ height: "30rem",width: '30rem' }} src={fadeImage.img} alt='images'/>
+                <h2>{fadeImage.caption}</h2>
+              </div>
+            ))}
+          </Fade>
         </div>
 
         <div id="linkContainer">
