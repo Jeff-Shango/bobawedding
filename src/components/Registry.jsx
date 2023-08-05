abbb719fea02fa83d00466922ed69e19c319af36
Jeff B <Jeff.BozierJr@gmail.com>
2023-08-05 08:01:40 -0400

    almost done back bone
    so excited to get to this point.
    
    trying to get the registry to show only on the registry
    
    add more ways to give money than just cashapp
    
    start working to get the pictures to appear on the timeline

--- src/components/Registry.jsx
+++ src/components/Registry.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const Registry = () => {
+
   return (
     <div>
       <h1 className="registryTitle">Bozier Registry</h1>
+      <button className="homeButton btn btn-primary" src="/" onClick={() => window.open('/', '_self')} >Go Home</button>
 
-      registryContainer
     </div>
   )
 }
