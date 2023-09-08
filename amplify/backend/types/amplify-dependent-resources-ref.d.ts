0d14b136154db242cc568c01fa2aeae7823f49a5
Jeff B <Jeff.BozierJr@gmail.com>
2023-09-08 11:32:18 -0400

    updating aws implement
    implementing through the documentation. hopefully it works

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "bobawedding6ba1fbaa": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
