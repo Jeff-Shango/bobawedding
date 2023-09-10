474b5d48961183a2a6220d638448a5bc6bdef849
Jeff B <Jeff.BozierJr@gmail.com>
2023-09-10 08:58:08 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from './App.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Amplify } from 'aws-amplify';
-import config from './aws-exports.js';
-Amplify.configure(config);
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
