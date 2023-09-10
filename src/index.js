9b2fb2a08227cd67dbd86d4aa6dfb38f413d13b1
Jeff B <Jeff.BozierJr@gmail.com>
2023-09-10 09:02:37 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
+import awsExports from './aws-exports.js';
 Amplify.configure(awsExports);
 
 
