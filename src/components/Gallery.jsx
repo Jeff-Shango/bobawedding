ff1412f11d6f7bff18c0a6dbb887a8fbe7977891
Jeff B <Jeff.BozierJr@gmail.com>
2023-09-12 11:18:11 -0400

    updating amplify stuff

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,7 @@ import img3 from '../assets/imgC.jpg';
 import "../App.css";
 import { Link } from 'react-router-dom';
 import axios from "axios";
-import Amplify,  {API}  from "aws-amplify"
+import {API}  from "aws-amplify"
 
 const myAPI = 'gallery';
 const path = '/gallery'
