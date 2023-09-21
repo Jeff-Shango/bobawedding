7eb51c867d0ee1ca5960147b8f42b0bb01608109
Jeff B <Jeff.BozierJr@gmail.com>
2023-09-21 19:56:13 -0400

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -44,7 +44,9 @@ const Gallery = () => {
 
     const fetchComments = async (imageId = null) => {
       try {
-        const response = await axios.get(`/get_comments/${imageId}`);
+        const apiUrl = `${window.location.origin}/get_comments/${imageId}`;
+        const response = await axios.get(apiUrl)
+        // const response = await axios.get(`/get_comments/${imageId}`);
         if (Array.isArray(response.data)) {
         const comments = response.data.map((item) => ({
           comments: item.comments,
