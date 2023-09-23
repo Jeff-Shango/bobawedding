031a9194bf08ec2922fdadcd3a4d5a8678f692b3
Jeff B <Jeff.BozierJr@gmail.com>
2023-09-23 08:23:18 -0400

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -43,19 +43,23 @@ const Gallery = () => {
   };
 
     const fetchComments = async (imageId = null) => {
-        // const apiUrl = `${window.location.origin}/get_comments/${imageId}`;
-        // const response = await axios.get(apiUrl)
-        const response = await axios.get(`/get_comments/${imageId}`);
-        // if (Array.isArray(response.data)) {
+      try {
+        const apiUrl = `${window.location.origin}/get_comments/${imageId}`;
+        const response = await axios.get(apiUrl)
+        // const response = await axios.get(`/get_comments/${imageId}`);
+        if (Array.isArray(response.data)) {
           const comments = response.data.map((item) => ({
             comments: item.comments,
             commentator: item.commentator,
           }));
           setCommentsData(comments);
-          console.log('this is what is coming up now: ' + response.data)
-      //   } else {
-      //   console.error("The Response isn't an array:", response.data);
-      // }
+        } else {
+        console.error("The Response isn't an array:", response.data);
+        setCommentsData([])
+      } 
+    } catch (err) {
+        console.log(err)
+      }
     };
 
 
