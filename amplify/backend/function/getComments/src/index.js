2785167e477e8cb93b883e313a1a90b42fb5bc16
Jeff B <Jeff.BozierJr@gmail.com>
2023-09-24 23:57:46 -0400

    Update index.js

--- amplify/backend/function/getComments/src/index.js
+++ amplify/backend/function/getComments/src/index.js
@@ -48,6 +48,5 @@ exports.handler = async (event) => {
     //      "Access-Control-Allow-Origin": "*",
     //      "Access-Control-Allow-Headers": "*"
     //  },
-        body: JSON.stringify(response),
     };
 };
