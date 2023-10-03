3fb27c5acfc161269ee5c4da81aa3bc0f6d92adf
Jeff B <Jeff.BozierJr@gmail.com>
2023-10-02 22:34:35 -0400

    updating

--- src/Ably.js
+++ src/Ably.js
@@ -1,4 +1,4 @@
 // import { Realtime } from "ably/browser/static/ably-commonjs.js"
 import { Realtime } from "ably"
 
-export default new Realtime(process.env.REACT_APP_ABLY_API_KEY)
+export default new Realtime(process.env.ABLY_API_KEY)
