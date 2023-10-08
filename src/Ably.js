fa84c0b0d3a1d39c9049483c7a77e6b568972e2d
Jeff B <Jeff.BozierJr@gmail.com>
2023-10-08 14:23:12 -0400

    successful local

--- src/Ably.js
+++
@@ -1,4 +0,0 @@
-// import { Realtime } from "ably/browser/static/ably-commonjs.js"
-import { Realtime } from "ably"
-
-export default new Realtime(process.env.REACT_APP_ABLY_API_KEY)
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,69 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import img1 from '../assets/imgA.jpg';
 import img2 from '../assets/imgB.jpg';
 import img3 from '../assets/imgC.jpg';
 import "../App.css";
-import { Link, useLocation } from 'react-router-dom';
-import axios from "axios";
-import Ably from '../Ably.js';
-import Comments from './Comments.js';
-// import { generateAblyToken } from '../serverFunctions/ablyFunctions.js';
-import { generateAblyToken } from '../serverFunctions/ablyFunction.jsx';
+import { Link } from 'react-router-dom';
+import WedComments from '../weddingCommentServer/Comments.jsx';
+import "./galleryStyles.css"
 
-// var Ably = require('ably');
 const Gallery = () => {
-  const location = useLocation();
+  // const location = useLocation();
   const imgContainer = [img1, img2, img3];
   const [enlargedImage, setEnlargedImage] = useState(null);
-  const [comments, setComments] = useState([]);
-  const [commentator, setCommentator] = useState('');
+  // const [comments, setComments] = useState([]);
+  // const [commentator, setCommentator] = useState('');
 
-  const generateToken = async () => {
-    try {
-      // const clientId = 'unique-client-id';
-      const response = await generateAblyToken({ clientId: 'unique-client-id' });
-      // const response = await generateAblyToken({ clientId: 'unique-client-id' });
-      const { tokenRequest } = response.data;
-      // Use the generated tokenRequest as needed in your application
-      console.log('Generated Token Request:', tokenRequest);
-    } catch (error) {
-      console.error('Error generating Ably token:', error);
-    }
-  };
-
-  useEffect(() => {
-    if (location.pathname === '/gallery') {
-    const fetchHistoricalComments = async () => {
-      const channel = Ably.channels.get("comments");
-      channel.attach();
-      channel.once("attached", () => {
-        channel.history((err, page) => {
-          if (!err) {
-            const historicalComments = Array.from(page.items, (item) => item.data);
-            setComments(historicalComments);
-          }
-        });
-      });
-    };
-    fetchHistoricalComments();
-  }
-}, [location]);
+//   useEffect(() => {
+//     if (location.pathname === '/gallery') {
+//     const fetchHistoricalComments = async () => {
+//       const channel = Ably.channels.get("comments");
+//       channel.attach();
+//       channel.once("attached", () => {
+//         channel.history((err, page) => {
+//           if (!err) {
+//             const historicalComments = Array.from(page.items, (item) => item.data);
+//             setComments(historicalComments);
+//           }
+//         });
+//       });
+//     };
+//     fetchHistoricalComments();
+//   }
+// }, [location]);
 
   // const handleChange = (e) => {
   //   if (e.target.name === 'comments') {
-  //     setCommentator(e.target.value);
-  //   } else if (e.target.name === 'commentator') {
   //     setComments(e.target.value);
+  //   } else if (e.target.name === 'commentator') {
+  //     setCommentator(e.target.value);
   //   }
   // };
-
-  const handleCommentChange = (e) => {
-    setComments(e.target.value);
-  };
-
-  const handleCommentatorChange = (e) => {
-    setCommentator(e.target.value);
-  }
 
   const handleImageClick = (image, index) => {
     console.log("Image clicked:", image);
@@ -71,8 +46,8 @@ const Gallery = () => {
       setEnlargedImage(null);
     } else {
       setEnlargedImage(image);
-      setComments('');
-      setCommentator('');
+      // setComments('');
+      // setCommentator('');
     }
   };
 
@@ -80,33 +55,33 @@ const Gallery = () => {
     setEnlargedImage(null);
   };
 
-  const addComment = async (commentText, commentatorName) => {
-    // e.preventDefault();
-    const comment = commentText.trim();
-    const name = commentatorName.trim();
-    // const comment = e.target.elements.comments.value.trim();
-    // const name = e.target.elements.commentator.value.trim();
-    const timestamp = Date.now();
+  // const addComment = async (commentText, commentatorName) => {
+  //   // e.preventDefault();
+  //   const comment = commentText.trim();
+  //   const name = commentatorName.trim();
+  //   // const comment = e.target.elements.comments.value.trim();
+  //   // const name = e.target.elements.commentator.value.trim();
+  //   const timestamp = Date.now();
 
-    const avatar = await (
-      await axios.get("https://dog.ceo/api/breeds/image/random")
-    ).data.message;
+  //   const avatar = await (
+  //     await axios.get("https://dog.ceo/api/breeds/image/random")
+  //   ).data.message;
 
-    if (name && comment) {
-      const commentObject = { name, comment, timestamp, avatar };
-      const channel = Ably.channels.get("comments");
-      channel.publish("add_comment", commentObject, (err) => {
-        if (err) {
-          console.log("Unable to publish message err = " + err.message);
-        }
-      });
-      setComments((prevComments) => [ commentObject, ...prevComments]);
-      // setComment('');
-      setCommentator('');
-      // e.target.elements.commentator.value = "";
-      // e.target.elements.comments.value = "";
-    }
-  };
+  //   if (name && comment) {
+  //     const commentObject = { name, comment, timestamp, avatar };
+  //     const channel = Ably.channels.get("comments");
+  //     channel.publish("add_comment", commentObject, (err) => {
+  //       if (err) {
+  //         console.log("Unable to publish message err = " + err.message);
+  //       }
+  //     });
+  //     setComments((prevComments) => [ commentObject, ...prevComments]);
+  //     // setComments('');
+  //     setCommentator('');
+  //     // e.target.elements.commentator.value = "";
+  //     // e.target.elements.comments.value = "";
+  //   }
+  // };
 
   return (
     <div>
@@ -136,9 +111,9 @@ const Gallery = () => {
           <div className='column is-half is-offset-one-quarter'>
             <h1 className="title">Please leave your feedback below</h1>
             {/* <form onSubmit={addComment}> */}
-            <form onSubmit={(e) => {
+            {/* <form onSubmit={(e) => {
               e.preventDefault();
-              addComment(commentator, comments);
+              // addComment(e.target.elements.comments.value, e.target.elements.commentator.value);
               // e.target.elements.commentator.value = "";
               // e.target.elements.comments.value = "";
             }}>
@@ -150,7 +125,7 @@ const Gallery = () => {
                     name="commentator"
                     placeholder="Your name"
                     value={commentator}
-                    onChange={handleCommentatorChange}
+                    onChange={handleChange}
                   />
                 </div>
               </div>
@@ -161,21 +136,21 @@ const Gallery = () => {
                     name="comments"
                     placeholder="Add a comment"
                     value={comments}
-                    onChange={handleCommentChange}
+                    onChange={handleChange}
                   ></textarea>
                 </div>
               </div>
               <div className="field">
                 <div className="control">
-                  <button onClick={generateToken} type="submit" className="button is-primary">
+                  <button type="submit" className="button is-primary">
                     Submit
                   </button>
                 </div>
               </div>
-            </form>
+            </form> */}
           </div>
           <section className="containerComment">
-            <Comments comments={comments} />
+            <WedComments currentUserId="1" /> 
           </section>
         </div>
       )}
@@ -184,4 +159,3 @@ const Gallery = () => {
 };
 
 export default Gallery;
-
