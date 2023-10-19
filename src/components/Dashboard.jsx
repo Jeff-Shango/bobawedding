c980b3fae43fb980280ce67e2a8447887cb56b9f
Jeff B <Jeff.BozierJr@gmail.com>
2023-10-19 08:30:18 -0400

    updating

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 // import { ProductCard } from '../functions/ProductCard.js';
 import './Navlinks/navlinks.css'
 import 'react-slideshow-image/dist/styles.css';
