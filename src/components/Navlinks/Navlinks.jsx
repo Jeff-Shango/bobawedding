a3fc66fe41e52654cc51f0c98a49c3d83fa1798f
Jeff B <Jeff.BozierJr@gmail.com>
2023-10-21 12:55:22 -0400

    k

--- src/components/Navlinks/Navlinks.jsx
+++ src/components/Navlinks/Navlinks.jsx
@@ -3,36 +3,39 @@ import {FaFileMedical} from 'react-icons/fa'
 // import {RiBoxingFill} from 'react-icons/ri'
 import {BsPersonFillAdd} from 'react-icons/bs'
 import {GiPapers} from 'react-icons/gi'
+// import { AiOutlineHome, FaFileMedical } from 'react-icons/ai';
+// import { BsPersonFillAdd } from 'react-icons/bs';
+// import { GiPapers } from 'react-icons/gi';
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-// import { AiOutlineHome, RiBoxingFill, BsPersonFillAdd, GiPapers, FaFileMedical } from 'react-icons/all';
-import "./navlinks.css";
+import { Link } from 'react-router-dom'; // Import Link from React Router
+import './navlinks.css';
 
 const Navlinks = () => {
   return (
     <Navbar expand="lg" variant="dark" id="mainNavContainer">
-      <Navbar.Brand href="/" id="logoLink">
+      <Navbar.Brand to="/" id="logoLink" as={Link}>
         {/* Your logo or brand */}
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto navContainer">
-          <Nav.Link id="dashLink" href="https://bozierwedding.netlify.app/">
+          <Link id="dashLink" to="https://bozierwedding.netlify.app/">
             <AiOutlineHome /> Home
-          </Nav.Link>
-          <Nav.Link id="dashLink" href="https://bozierwedding.netlify.app/schedule">
+          </Link>
+          <Link id="dashLink" to="https://bozierwedding.netlify.app/schedule">
             <BsPersonFillAdd /> Schedule
-          </Nav.Link>
-          <Nav.Link id="dashLink" href="https://bozierwedding.netlify.app/weddingParty">
+          </Link>
+          <Link id="dashLink" to="https://bozierwedding.netlify.app/weddingParty">
             <GiPapers /> Wedding Team
-          </Nav.Link>
-          <Nav.Link id="dashLink" href="https://bozierwedding.netlify.app/registry">
+          </Link>
+          <Link id="dashLink" to="https://bozierwedding.netlify.app/registry">
             <FaFileMedical /> Registry
-          </Nav.Link>
+          </Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
-}
+};
 
 export default Navlinks;
