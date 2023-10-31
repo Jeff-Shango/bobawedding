43dab2c8f2579c8192605fd5c4e1045eb0ed81ec
Jeff B <Jeff.BozierJr@gmail.com>
2023-10-31 16:14:18 -0400

    updating

--- src/components/Registry.jsx
+++ src/components/Registry.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import "./registry/registryStyling.css";
 // import {Row, Col} from 'react-bootstrap';
 // import { productsArray } from '../functions/stripeFunctions';
@@ -7,15 +7,15 @@ import Navlinks from './Navlinks/Navlinks.jsx'
 
 const Registry = () => {
   
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = '//www.myregistry.com//Visitors/GiftList/iFrames/EmbedRegistry.ashx?r=hvFHaT5YimquHhrHtXG1yQ2&v=2';
-    document.getElementById('scriptContainer').appendChild(script);
+  // useEffect(() => {
+  //   const script = document.createElement('script');
+  //   script.src = '//www.myregistry.com//Visitors/GiftList/iFrames/EmbedRegistry.ashx?r=hvFHaT5YimquHhrHtXG1yQ2&v=2';
+  //   document.getElementById('scriptContainer').appendChild(script);
 
-    return () => {
-      document.getElementById('scriptContainer').removeChild(script);
-    };
-  }, []);
+  //   return () => {
+  //     document.getElementById('scriptContainer').removeChild(script);
+  //   };
+  // }, []);
 
 
   return (
@@ -24,17 +24,20 @@ const Registry = () => {
     <div id='registryPageContainer'>
               {/* Registry Link Container */}
 
-              <div className="registryContainer">
+              {/* <div className="registryContainer"> */}
 
 
   <div className="cashAppContainer">
     <img src={cashApp} alt="cashApp" />
-    <aside id='registryAside'>Scan or click on the links</aside>
+    <aside id='registryAside'>Scan or click on the links to donate or see registry</aside>
     <button src="https://cash.app/$TheBoziers" className='cashAppButton' onClick={() => window.open('https://cash.app/$TheBoziers', '_blank')}>
       Cash App
     </button>
     <button src="https://enroll.zellepay.com/qr-codes?data=eqwoglCJ0b" className='cashAppButton' onClick={() => window.open('https://enroll.zellepay.com/qr-codes?data=ewogICJ0b2tlbiIgOiAiNDEwNTcwODU3OCIsCiAgImFjdGlvbiIgOiAicGF5bWVudCIsCiAgIm5hbWUiIDogIkFTSExFWSIKfQ==', '_blank')}>
       Zelle
+    </button>
+    <button src="https://www.myregistry.com/wedding-registry/jeff-bozier-and-ashley-barney-baltimore-md/3857459" className='cashAppButton' onClick={() => window.open('https://www.myregistry.com/wedding-registry/jeff-bozier-and-ashley-barney-baltimore-md/3857459', '_blank')}>
+      Official Registry
     </button>
   </div>
 
@@ -55,13 +58,7 @@ const Registry = () => {
 {/* <NavbarComponent></NavbarComponent> */}
 {/* end of navbar */}
 
-</div>
 {/* End of Registry Portion */}
-
-<iframe
-id='script_myregistry_giftlist_iframe'
-title="MyRegistry Gift List"
-></iframe>
 
 
     </div>
