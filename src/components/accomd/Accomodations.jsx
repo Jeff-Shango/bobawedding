7d99592a0aa07e3faeeb747d2f3b43e167370c36
Jeff B <jeff.bozierjr@gmail.com>
2023-12-30 21:02:08 -0500

    updating

--- src/components/accomd/Accomodations.jsx
+++ src/components/accomd/Accomodations.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../accomd/accomodations.css';
+import Navlinks from '../Navlinks/Navlinks';
 
 const Accomodations = () => {
   const bozierLink = process.env.REACT_APP_BozierLink || "";
 
   return (
     <div id='accomdPage'>
+      <Navlinks/>
       <a href="https://www.hyatt.com/en-US/hotel/maryland/hyatt-place-baltimore-inner-harbor/bwizh?corp_id=G-BARN" className="accomdLink" target="_blank">
         BozierEverAfter
       </a>        
