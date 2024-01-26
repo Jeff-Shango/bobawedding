0d086e5be96421c2afb9fed015b6d85c772f5710
Jeff B <jeff.bozierjr@gmail.com>
2024-01-25 23:00:40 -0500

    UPDATING

--- src/components/accomd/Accomodations.jsx
+++ src/components/accomd/Accomodations.jsx
@@ -8,11 +8,11 @@ const Accomodations = () => {
   return (
     <div id='accomdPage'>
       <Navlinks/>
-      <h1 className="accomdTopTitle">Venue</h1>
+      {/* <h1 className="accomdTopTitle">Venue</h1> */}
 
       <div className="accomdTopLayer">
-        <h2 className="accomdTopLayerContent">The ceremony and celebration will be held at <strong>Celebrations at the Bay: Tented Vista Ballroom.</strong> 2042 Knollview Ave. Pasadena, MD 21122
-        </h2>
+        {/* <h2 className="accomdTopLayerContent">The ceremony and celebration will be held at <strong>Celebrations at the Bay: Tented Vista Ballroom.</strong> 2042 Knollview Ave. Pasadena, MD 21122
+        </h2> */}
 
         <div className='topAccomdPic'/>
       </div>
@@ -20,7 +20,7 @@ const Accomodations = () => {
       <div className="accomdBottomLayer">
         <h1 className="accomdBottomTitle">Accomodations</h1>
 
-        <h2 className="accomdBottomLayerContent">
+        {/* <h2 className="accomdBottomLayerContent">
           For your convenience, a block of rooms has been reserved at the Hyatt Place Baltimore Inner Harbor, about 30 minutes from the venue. Reservations will be made by individuals before April 30, 2024 by using our custom booking link: <u>BozierEverAfter</u>
           <br/>
           or
@@ -28,7 +28,7 @@ const Accomodations = () => {
           Calling 1-888 HYATT HP (1-888-492-8847) and referencing the Group Code: <strong>G-BARN</strong>
           <br/>
           *Please note, all reservations must be made by the cutoff date April 30, 2024 to be guaranteed the negotiated group rate.
-        </h2>
+        </h2> */}
 
         <div className="bottomAccomdPic"></div>
         </div>
