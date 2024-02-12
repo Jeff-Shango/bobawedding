0fb59198ad7a33b2bdfb562a7d657dc1974c8d46
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2024-02-12 13:07:45 -0500

    Update RSVP.jsx

--- src/components/RSVP.jsx
+++ src/components/RSVP.jsx
@@ -34,7 +34,7 @@ const RSVP = () => {
     
       <div className="formNameContainer">
         <div className="form">
-          <input type="text" name="firstName" autoComplete='off' required className='inputFName'/>
+          <input type="text" name="name" autoComplete='off' required className='inputFName'/>
             <label htmlFor="text" className="label-name labelFName">
               <span className="content-name spanFName">
                 First Name
