5fc043caa26a359b2623b964e85b8ac96ec9b540
Jeff B <jeff.bozierjr@gmail.com>
2024-02-20 09:35:30 -0500

    updating

--- src/components/RSVP.jsx
+++ src/components/RSVP.jsx
@@ -26,7 +26,7 @@ const RSVP = () => {
       
     <h1 className="rsvpTitle">RSVP</h1>
 
-    <h2 className="rsvpSubTitle">RSVP Due by:April 30, 2024</h2>
+    <h2 className="rsvpSubTitle">RSVP Due by:April 21, 2024</h2>
 
     <p className="rsvpP">We warmly invite you to RSVP for The Bozier wedding! Please don't hesistate to contact either Ashley or Jeff if you have any questions or need assistance. We're looking forward to seeing all of you there and sharing this joyous occasion together!!</p>
 
