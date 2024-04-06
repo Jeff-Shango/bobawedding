593c90cd14d148f2b2dbf316f18b8d5333acb3ea
Jeff B <jeff.bozierjr@gmail.com>
2024-04-06 14:27:38 -0400

    updating

--- src/components/Registry.jsx
+++ src/components/Registry.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./registry/registryStyling.css";
 // import {Row, Col} from 'react-bootstrap';
 // import { productsArray } from '../functions/stripeFunctions';
 // import cashApp from '../assets/theBoziersCashApp.png';
-import bozierMoneyTag from '../assets/bozierMoneyTag.png';
+import bozierMoneyTag from '../assets/borderWOQR.png';
 import Navlinks from './Navlinks/Navlinks.jsx'
 
 const Registry = () => {
-  
+  const [buttons, setButtons] = useState([
+    { label: 'Cash App', link: 'https://cash.app/$TheBoziers' },
+    { label: 'Zelle', link: 'https://enroll.zellepay.com/qr-codes?data=eqwoglCJ0b' },
+    { label: 'Official Registry', link: 'https://www.myregistry.com/wedding-registry/jeff-bozier-and-ashley-barney-baltimore-md/3857459' }
+  ]);
+
   // useEffect(() => {
   //   const script = document.createElement('script');
   //   script.src = '//www.myregistry.com//Visitors/GiftList/iFrames/EmbedRegistry.ashx?r=hvFHaT5YimquHhrHtXG1yQ2&v=2';
@@ -22,17 +27,36 @@ const Registry = () => {
   return (
     <>
     <Navlinks/>
-    <div id='registryPageContainer'>
+    {/* <div id='registryPageContainer'> */}
+    <div id='registryPageContainer' style={{ backgroundImage: `url(${bozierMoneyTag})` }}>
+        {/* Description */}
+
               {/* Registry Link Container */}
 
               {/* <div className="registryContainer"> */}
 
 
-  <div className="cashAppContainer">
+  {/* <div className="cashAppContainer"> */}
     <h3 className="registryDescriptionTop">Your presence on our special day means the world to us, and we are truly grateful for your love and support. If you are considering a gift, a contribution to our future together would be greatly appreciated.</h3>
-    <img id='moneyImg' src={bozierMoneyTag} alt="cashApp" />
+
+         {/* Button list */}
+         <div className="cashAppContainer">
+          {buttons.map((button, index) => (
+            <button
+              key={index}
+              className='cashAppButton'
+              onClick={() => window.open(button.link, '_blank')}
+            >
+              {button.label}
+            </button>
+          ))}
+        </div>
+
+    {/* <img id='moneyImg' src={bozierMoneyTag} alt="cashApp" /> */}
     <h3 className="registryDescriptionBottom">We understand that some of our guests may prefer to give traditional wedding gifts. In that case, please use the following links to acces our registry.</h3>
-    <button src="https://cash.app/$TheBoziers" className='cashAppButton' id='cashAppButtonA' onClick={() => window.open('https://cash.app/$TheBoziers', '_blank')}>
+
+    
+    {/* <button src="https://cash.app/$TheBoziers" className='cashAppButton' id='cashAppButtonA' onClick={() => window.open('https://cash.app/$TheBoziers', '_blank')}>
       Cash App
     </button>
     <button src="https://enroll.zellepay.com/qr-codes?data=eqwoglCJ0b" className='cashAppButton' id='cashAppButtonB' onClick={() => window.open('https://enroll.zellepay.com/qr-codes?data=ewogICJ0b2tlbiIgOiAiNDEwNTcwODU3OCIsCiAgImFjdGlvbiIgOiAicGF5bWVudCIsCiAgIm5hbWUiIDogIkFTSExFWSIKfQ==', '_blank')}>
@@ -40,7 +64,7 @@ const Registry = () => {
     </button>
     <button src="https://www.myregistry.com/wedding-registry/jeff-bozier-and-ashley-barney-baltimore-md/3857459" className='cashAppButton' id='cashAppButtonC' onClick={() => window.open('https://www.myregistry.com/wedding-registry/jeff-bozier-and-ashley-barney-baltimore-md/3857459', '_blank')}>
       Official Registry
-    </button>
+    </button> */}
   </div>
 
   {/* <div className="zelleContainer">
@@ -63,7 +87,7 @@ const Registry = () => {
 {/* End of Registry Portion */}
 
 
-    </div>
+    {/* </div> */}
     </>
   )
 }
